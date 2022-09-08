094ab665bfa2133f25d9b2cb62d308cd3115928c
Konstantin <gusarov_konstant@mail.ru>
2022-09-09 02:54:58 +0300

    03

---
+++ HW3/1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число:");
+int x = int.Parse(Console.ReadLine()); 
+char[] arr = x.ToString().ToCharArray();
+
+int x1 = arr[0];
+int x2 = arr[1];
+int x4 = arr[3];
+int x5 = arr[4];
+if (x1==x5 && x2==x4) {
+    Console.WriteLine("Это палиндром");
+}
+else Console.WriteLine("Это не палиндром");
+
+
---
+++ HW3/3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+double f (int n) {
+for (int i = 0; i<=n; i++)
+{
+double res = Math.Pow(i, 3);
+return res;
+}
+ 
+}
+
+Console.Write("Введите цифру:");
+int n = int.Parse(Console.ReadLine()); 
+
+double res = f(n); 
+Console.Write(res);
+
