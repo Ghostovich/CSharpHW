8c3e636fef108441f3e8560ce3dc2ddd8785e175
Konstantin <gusarov_konstant@mail.ru>
2022-09-10 14:59:46 +0300

    04

---
+++ HW4/1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. Решить задачу с использованием методов.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите пятизначное число:");
+int x = int.Parse(Console.ReadLine()); 
+char[] arr = x.ToString().ToCharArray();
+
+int x1 = arr[0];
+int x2 = arr[1];
+int x4 = arr[3];
+int x5 = arr[4];
+if (x1==x5 && x2==x4) {
+    Console.WriteLine("Это палиндром");
+}
+else Console.WriteLine("Это не палиндром");
+
+
---
+++ HW4/2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите координаты X, Y, Z для точки 1:");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1  = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты X, Y, Z для точки 2:");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2));
+Console.Write($"Расстояние мужду точками 1 и 2 равно: {d}");
+
+
+
