4072b6088a093c70845815f186060bbb07ddec42
Konstantin <gusarov_konstant@mail.ru>
2022-09-26 21:50:52 +0300

    07.1

--- HW7/1/Program.cs
+++ HW7/1/Program.cs
@@ -3,3 +3,29 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+int m = new Random().Next(1,10);
+int n = new Random().Next(1,10);
+
+double[,] GetArray (int m, int n)
+{   
+    double[,] array = new double[n, m];
+    for (int i=0; i<n;i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+           array [i,j] = new Random().Next(-10,10)/10.0;
+        }
+    }
+    return array;
+}
+
+double[,] arr = GetArray(m, n);
+for (int i=0; i<n;i++)
+{
+    for (int j=0; j<m; j++)
+    {
+        Console.Write(arr [i,j]+" ");
+    }
+    Console.WriteLine();
+}
--- HW7/3/Program.cs
+++ HW7/3/Program.cs
@@ -4,3 +4,53 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// int m = new Random().Next(3,5);
+// int n = new Random().Next(3,5);
+
+int m = 3;
+int n = 4;
+
+int[,] GetArray (int n, int m)
+{
+    int[,] array = new int[m, n];
+    for (int i=0; i<m;i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+           array [i,j] = new Random().Next(-10,10);
+        }
+    }
+    return array;
+}
+
+double[] GetMiddle(int[,] arr, int n, int m)
+{   
+    double[] mid = new double[n];
+    int sum = 0;
+    for (int j=0; j<n; j++)
+    {
+        for (int i=0; i<m; i++)
+        {
+            sum = sum+arr[i,j];
+        }
+        mid[j] = (double)sum/m;
+        mid[j] = Math.Round(mid[j],1);
+        sum = 0;
+    }   
+    return mid;
+}
+
+int[,] arr = GetArray(n,m);
+for (int i=0; i<m; i++)
+{
+    for (int j=0; j<n; j++)
+    {
+        Console.Write(arr [i,j]+" ");
+    }
+    Console.WriteLine();
+}
+double[] printMid = GetMiddle(arr, n, m);
+Console.WriteLine($"Среднее арифметическое каждого столбца: ");
+Console.Write(string.Join(" ", printMid));
+
